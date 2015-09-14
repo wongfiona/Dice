489a7227a05931c1fe17bccd9d561018f960e2f4
wongfiona <wong.fiona.11@gmail.com>
2015-09-14 11:42:09 -0700

    3 Dots

--- Dice.pde
+++ Dice.pde
@@ -8,9 +8,9 @@ void setup()
 
 void draw()
 {
-	for(int y = 0; y <= 500; y += 50)
+	for(int y = 0; y <= 400; y += 75)
 	{
-		for(int x = 0; x <= 500; x += 50)
+		for(int x = 0; x <= 500; x += 75)
 		{
 			bob = new Die(x,y);
 			bob.show();
@@ -29,23 +29,40 @@ class Die
 	int myX, myY, dots;
 	Die(int x, int y)
 	{
+		show();
 		roll();
 		myX = x;
 		myY = y;
 
 	}
+
 	void roll()
 	{
-		int dots = (int)(Math.random()*0)+1;
-		if(dots == 1){
+		int dots = (int)(Math.random()*3)+1;
+		if (dots == 1){
 			fill(0);
-			ellipse(25,25,10,10);
+			ellipse(myX+25, myY+25, 10, 10);
 		}
-			
+		else if (dots == 2){
+			fill(0);
+			ellipse(myX+15, myY+15, 10, 10);
+			ellipse(myX+35, myY+35, 10, 10);
+		}
+		else if (dots == 3){
+			fill(0);
+			ellipse(myX+10, myY+10, 10, 10);
+			ellipse(myX+25, myY+25, 10, 10);
+			ellipse(myX+40, myY+40, 10, 10);
+		}
+
+
 	}
+
 	void show()
 	{
 		fill(255, 255, 255);
-		rect(myX,myY,50,50,20);
+		rect(myX,myY,50,50,10);
+		roll();
 	}
+	
 }
--- build-tmp/source/Dice.java
+++ build-tmp/source/Dice.java
@@ -24,9 +24,9 @@ public void setup()
 
 public void draw()
 {
-	for(int y = 0; y <= 500; y += 50)
+	for(int y = 0; y <= 400; y += 75)
 	{
-		for(int x = 0; x <= 500; x += 50)
+		for(int x = 0; x <= 500; x += 75)
 		{
 			bob = new Die(x,y);
 			bob.show();
@@ -45,25 +45,42 @@ class Die
 	int myX, myY, dots;
 	Die(int x, int y)
 	{
+		show();
 		roll();
 		myX = x;
 		myY = y;
 
 	}
+
 	public void roll()
 	{
-		int dots = (int)(Math.random()*0)+1;
-		if(dots == 1){
+		int dots = (int)(Math.random()*3)+1;
+		if (dots == 1){
 			fill(0);
-			ellipse(25,25,10,10);
+			ellipse(myX+25, myY+25, 10, 10);
 		}
-			
+		else if (dots == 2){
+			fill(0);
+			ellipse(myX+15, myY+15, 10, 10);
+			ellipse(myX+35, myY+35, 10, 10);
+		}
+		else if (dots == 3){
+			fill(0);
+			ellipse(myX+10, myY+10, 10, 10);
+			ellipse(myX+25, myY+25, 10, 10);
+			ellipse(myX+40, myY+40, 10, 10);
+		}
+
+
 	}
+
 	public void show()
 	{
 		fill(255, 255, 255);
-		rect(myX,myY,50,50,20);
+		rect(myX,myY,50,50,10);
+		roll();
 	}
+	
 }
   static public void main(String[] passedArgs) {
     String[] appletArgs = new String[] { "Dice" };
