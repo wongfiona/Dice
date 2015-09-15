4ad8b7f65b0f56052528a26c627bdb216523ff09
wongfiona <wong.fiona.11@gmail.com>
2015-09-15 10:15:39 -0700

    Changed font

--- build-tmp/source/Dice.java
+++ build-tmp/source/Dice.java
@@ -18,69 +18,95 @@ Die bob;
 
 public void setup()
 {
-	size(500,500);
-	noLoop();
+  size(575,500);
+  noLoop();
 }
 
 public void draw()
 {
-	for(int y = 0; y <= 400; y += 75)
-	{
-		for(int x = 0; x <= 500; x += 75)
-		{
-			bob = new Die(x,y);
-			bob.show();
-		}
-	}
-	
+  int total = 0;
+  background(112, 190, 224);
+  for(int y = 20; y <= 340; y += 80)
+  {
+    for(int x = 20; x <= 500; x += 80)
+    {
+      bob = new Die(x,y);
+      bob.show();
+      total = total + bob.dots;
+    }
+  }
+  textSize(30);
+  fill(0);
+  text("Total is " + total, 190, 450);
+
 }
 
 public void mousePressed()
 {
-	redraw();
+  redraw();
 }
 
 class Die 
 {
-	int myX, myY, dots;
-	Die(int x, int y)
-	{
-		show();
-		roll();
-		myX = x;
-		myY = y;
+  int myX, myY, dots;
+  Die(int x, int y)
+  {
+    myX = x;
+    myY = y;
+    roll();
+  }
 
-	}
+  public void roll()
+  {
+    dots = (int)(Math.random()*6)+1;
+    if (dots == 1){
+      fill(0);
+      ellipse(myX+25, myY+25, 10, 10);
+    }
+   else if (dots == 2){
+      fill(0);
+      ellipse(myX+15, myY+15, 10, 10);
+      ellipse(myX+35, myY+35, 10, 10);
+    }
+    else if (dots == 3){
+      fill(0);
+      ellipse(myX+12, myY+12, 10, 10);
+      ellipse(myX+25, myY+25, 10, 10);
+      ellipse(myX+38, myY+38, 10, 10);
+    }
+    else if (dots == 4){
+      fill(0);
+      ellipse(myX+15, myY+15, 10, 10);
+      ellipse(myX+35, myY+15, 10, 10);
+      ellipse(myX+15, myY+35, 10, 10);
+      ellipse(myX+35, myY+35, 10, 10);
+    }
+    else if (dots == 5){
+      fill(0);
+      ellipse(myX+12, myY+12, 10, 10);
+      ellipse(myX+38, myY+12, 10, 10);
+      ellipse(myX+25, myY+25, 10, 10);
+      ellipse(myX+12, myY+38, 10, 10);
+      ellipse(myX+38, myY+38, 10, 10);
+    }
+    else if (dots == 6){
+      fill(0);
+      ellipse(myX+15, myY+11, 10, 10);
+      ellipse(myX+35, myY+11, 10, 10);
+      ellipse(myX+15, myY+25, 10, 10);
+      ellipse(myX+35, myY+25, 10, 10);
+      ellipse(myX+15, myY+39, 10, 10);
+      ellipse(myX+35, myY+39, 10, 10);
+    }
+    
+  }
 
-	public void roll()
-	{
-		int dots = (int)(Math.random()*3)+1;
-		if (dots == 1){
-			fill(0);
-			ellipse(myX+25, myY+25, 10, 10);
-		}
-		else if (dots == 2){
-			fill(0);
-			ellipse(myX+15, myY+15, 10, 10);
-			ellipse(myX+35, myY+35, 10, 10);
-		}
-		else if (dots == 3){
-			fill(0);
-			ellipse(myX+10, myY+10, 10, 10);
-			ellipse(myX+25, myY+25, 10, 10);
-			ellipse(myX+40, myY+40, 10, 10);
-		}
-
-
-	}
-
-	public void show()
-	{
-		fill(255, 255, 255);
-		rect(myX,myY,50,50,10);
-		roll();
-	}
-	
+  public void show()
+  {
+    fill(255, 255, 255);
+    rect(myX,myY,50,50,10);
+    roll();
+  }
 }
   static public void main(String[] passedArgs) {
     String[] appletArgs = new String[] { "Dice" };
